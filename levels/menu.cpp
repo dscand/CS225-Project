84e70ac31f56b9bfda41323613bd26ad63174861
Ilanacorn <63212852+Ilanacorn@users.noreply.github.com>
2022-10-24 14:16:18 -0700

    Update menu.cpp

--- levels/menu.cpp
+++ levels/menu.cpp
@@ -7,9 +7,6 @@ class LButton
 
         //Sets top left position
         void setPosition( int x, int y );
-
-        //Handles mouse event
-        void handleEvent( SDL_Event* e );
     
         //Shows button sprite
         void render();
