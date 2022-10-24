1a71867f462fc7b4ba024ccd1c15b54454a2877a
Ilanacorn <63212852+Ilanacorn@users.noreply.github.com>
2022-10-24 12:44:29 -0700

    Update button.h

--- button.h
+++ button.h
@@ -1,17 +1,29 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-class Button{
-public:
-  SDL_Texture * tex;
-  SDL_Rect srect, drect;
-  bool isPressed = false;
+#include "SDL.h"
+#include <string>
+#include <vector>
+#include "Input.h"
+#include "GameObject.h"
+#include "TectureMhr.h"
+
+using TextureIdList = std::vector<std::string>;
+
+class Button : GameObject {
   
-  button();
-  virtual ~Button();
+  public:
   
-  void update(Mouse& mouse);
-  void draw();
+    button(int x, int y, void (*callback)(), TextureIdList textrIDs) : GameObject(new Transform()){
+      m_Tf-> X = x;
+      m_Tf-> Y = y;
+      m_Callback = callback;
+      m_TextrIDs = textrIDs;
+    }
+    virtual ~Button();
+  
+    void update(Mouse& mouse);
+    void draw();
 };
 
 #endif // BUTTON_H
