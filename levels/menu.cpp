09ffb61359fe0f5e1e8a8f878854ae88d37d7bbb
Ilanacorn <63212852+Ilanacorn@users.noreply.github.com>
2022-10-24 14:03:09 -0700

    created a menu.cpp for menu classes

---
+++ levels/menu.cpp
@@ -0,0 +1,36 @@
+// 
+class LButton
+{
+    public:
+        //Initializes internal variables
+        LButton();
+
+        //Sets top left position
+        void setPosition( int x, int y );
+
+        //Handles mouse event
+        void handleEvent( SDL_Event* e );
+    
+        //Shows button sprite
+        void render();
+
+    private:
+        //Top left position
+        SDL_Point mPosition;
+
+        //Currently used global sprite
+        LButtonSprite mCurrentSprite;
+};
+LButton::LButton()
+{
+    mPosition.x = 0;
+    mPosition.y = 0;
+
+    mCurrentSprite = BUTTON_SPRITE_MOUSE_OUT;
+}
+
+void LButton::setPosition( int x, int y )
+{
+    mPosition.x = x;
+    mPosition.y = y;
+}
