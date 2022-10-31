c16dca2c7064b0b53268de32a4854b223b4e8ba9
Ilanacorn <63212852+Ilanacorn@users.noreply.github.com>
2022-10-31 15:46:15 -0700

    Added comments, added reasoning for true

--- menu.cpp
+++ menu.cpp
@@ -2,11 +2,12 @@ namespace MouseFunctions {
 	bool mouseOver(int x, int y, int width, int height) {
 		int mouseX, mouseY;
 		SDL_GetMouseState( &mouseX, &mouseY );
+		// sets bounds for the texture
 		lowLimitX = x - (width/2);
 		upLimitX = x + (width/2);
 		lowLimitY = y - (height/2);
 		upLimitY = y + (height/2);
-		
+		// will tell you if the mouse position is within the bounds of the texture
 		if (mouseX >= lowLimitX & mouseX <= upLimitX & mouseY >= lowLimitY & mouseY <= upLimitY){
 			return true;
 		}
