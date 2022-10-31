f4dd6da344fbb9b93d36a72f789ffa2db141ef3a
Ilanacorn <63212852+Ilanacorn@users.noreply.github.com>
2022-10-31 15:42:50 -0700

    Finished TODO: return true if inside demensions

--- menu.cpp
+++ menu.cpp
@@ -2,9 +2,19 @@ namespace MouseFunctions {
 	bool mouseOver(int x, int y, int width, int height) {
 		int mouseX, mouseY;
 		SDL_GetMouseState( &mouseX, &mouseY );
+		/*
+		lowLimitX = x - (width/2);
+		upLimitX = x + (width/2);
+		lowLimitY = y - (height/2);
+		upLimitY = y + (height/2);
+		
+		if (mouseX >= lowLimitX & mouseX <= upLimitX & mouseY >= lowLimitY & mouseY <= upLimitY){
+			return true;
+		}
+		else{
+			return false;
+		}
+		*/
 
-		// TODO: return true if mouse inside dimentions
-
-		return false;
 	}
 }
