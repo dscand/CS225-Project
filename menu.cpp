11e3b414826c3cc68d7874640623ad5c1c577af5
Ilan Larkins <63212852+Ilanacorn@users.noreply.github.com>
2022-11-14 10:24:27 -0700

    Added Button Sound

--- menu.cpp
+++ menu.cpp
@@ -6,6 +6,7 @@ bool MouseFunctions::mouseOver(int x, int y, int width, int height) {
 
 	// will tell you if the mouse position is within the bounds of the texture
 	if (mouseX >= x - (width/2) && mouseX <= x + (width/2) && mouseY >= y - (height/4) && mouseY <= y + (height/4)) {
+		Mix_PlayChannel( -1, gSound[1] , 0 );
 		return true;
 	}
 	else return false;
