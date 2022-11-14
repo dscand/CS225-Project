b6b7f072f2f37d087f6794e1e3ec908f1c008e35
Ilan Larkins <63212852+Ilanacorn@users.noreply.github.com>
2022-11-14 13:00:41 -0700

    Added button sounds

--- levels/level_menu.cpp
+++ levels/level_menu.cpp
@@ -149,12 +149,16 @@ void _level_menu::step(Level* level) {
 					if (MouseFunctions::mouseOver(level->textures[buttonIndex]->getPosX(), level->textures[buttonIndex]->getPosY(), level->textures[buttonIndex]->getWidth(), level->textures[buttonIndex]->getHeight())) {
 						switch (buttonIndex) {
 							case 0:
+								// Play Sound effect
+								
 								// Play
 								level->nextLevel = 1;
 								level->stop = true;
 								break;
 
 							case 2:
+								// Play Sound effect
+								
 								// Exit
 								level->nextLevel = -1;
 								level->stop = true;
